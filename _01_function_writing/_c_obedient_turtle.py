0c12ddbfdc320e9e9b94c3e7cb407ab218e2a98a
ashwinsingh <ashwinjsingh012@gmail.com>
2024-01-28 12:59:16 -0800

    qwert

--- _01_function_writing/_c_obedient_turtle.py
+++ _01_function_writing/_c_obedient_turtle.py
@@ -4,31 +4,37 @@ Make an obedient turtle that will obey commands to draw shapes.
 from tkinter import messagebox, simpledialog, Tk
 import turtle
 
-def my_turtle(square):
+def Fuiyo():
 
     turtle.goto(x = 20, y = 50)
     turtle.pendown()
     turtle.forward(100)
-    turtle.left(90)
-    my_turtle.forward(100)
-    my_turtle.right(90)
-    my_turtle.forward(100)
-    my_turtle.right(90)
-    my_turtle.forward(100)
+    turtle.left(-90)
+    turtle.forward(100)
+    turtle.left(-90)
+    turtle.forward(100)
+    turtle.left(-90)
+    turtle.forward(100)
+    turtle.left(-90)
 
 
 
-def my_turtle_1(triangle):
-    my_turtle.goto(x=30, y=50)
+
+def tri():
     my_turtle.pendown()
-    my_turtle.left(60)
-    my_turtle.forward(100)
-    my_turtle.right(60)
-    my_turtle.forward(100)
-    my_turtle.right(60)
-    my_turtle.forward(100)
+    for i in range (3):
+        my_turtle.forward(100)
+        my_turtle.right(120)
 
 
+
+
+
+
+
+def circ():
+    rad = 100
+    my_turtle.circle(rad)
 
 
 
@@ -41,7 +47,17 @@ if __name__ == '__main__':
     #      circle.
 
     #   3. Ask the user for the for a shape to draw.
+    de_donde_es = simpledialog.askstring(title='hola ',prompt='what shape do u want to draw (a square, circle or a triangle r ur options, aswell no caps)')
 
     #   4. Draw the appropriate shape depending on their answer (call the right
     #      function)
+    if de_donde_es == 'square':
+        Fuiyo()
+    if de_donde_es == 'triangle':
+        tri()
+    if de_donde_es == 'circle':
+        circ()
+
+    turtle.done()
     pass
+
